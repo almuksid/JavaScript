02eb9645e305990ac5c2d157cc170b8c0c9f50af
Al Muksid <deveelopment.almuksid@gmail.com>
2025-10-02 17:17:45 +0600

    If 50 to 300 it using 15% otherwize 20%. Udemy challenge.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log("JavaScript is easy to learn")
+// console.log("JavaScript is easy to learn")
+
+// Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%
+
+/* Write your code below. Good luck! 🙂 */
+const calcTip = function(bill) {
+    return bill >= 50 && bill <=300 ? bill * .15 : bill * .20;
+}
+
+const bills = [125, 555, 44]
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+
+const total =[ bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2], ]
+
+console.log(bills, tips, total);
