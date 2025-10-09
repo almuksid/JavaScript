aeb24c8d6658d7f8dddd03cee6a9e9458b8d603b
Al Muksid <deveelopment.almuksid@gmail.com>
2025-10-09 14:06:24 +0600

    Switch Case

---
+++ javascript/switchCase.js
@@ -0,0 +1,29 @@
+const day = "Thusday";
+
+switch(day){
+    case "Sunday":
+        console.log("Group study with our new team.");
+        break;
+    case "Monday":
+        console.log("JavaScript practice in at least 8 hours.");
+        break;
+    case "Tuesday":
+        console.log("Reading Book.");
+        break;
+    case "Wednesday":
+        console.log("Reading Book.");
+        break;
+    case "Thusday":
+        console.log("JavaScript practice in at least 8 hours & Group study with our team member.");
+        break;
+    case "Friday":
+        console.log("JavaScript practice in at least 8 hours.");
+        break;
+    case "Saturday":
+        console.log("JavaScript practice in at least 8 hours.");
+        break;
+    default:
+        console.log("Invalid Code.")
+        break;
+    
+}
