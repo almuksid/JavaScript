02d0ba486e667436de6c5a5921040dd648330690
Al Muksid <deveelopment.almuksid@gmail.com>
2025-10-09 15:31:15 +0600

    Ternary Operator

---
+++ javascript/ternaryOperator.js
@@ -0,0 +1,8 @@
+console.log("Ternary Operator")
+const age = 82;
+age>= 18 ? console.log("You can Vote") : console.log("You Can't Vote")
+
+const drink = age >= 18 ? "You can drink wine🍷" : "You drink water"
+console.log(drink);
+
+console.log(`You drink ${age >= 18 ? "Wine🍷": "Water 🚰" }`)
