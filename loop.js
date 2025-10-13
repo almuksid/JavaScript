586cf7a8fa37fcff1b41b4f9e9bfa9e22ced40bd
Al Muksid <deveelopment.almuksid@gmail.com>
2025-10-14 00:46:04 +0600

    For Loop

---
+++ loop.js
@@ -0,0 +1,11 @@
+// For Loop
+
+for(let rep = 1; rep<=10; rep++){
+    console.log(`Lifting weight repitition ${rep} 🏋️‍♂️. `)
+}
+
+const language = ["JavaScript", "Python", "C++", ["HTML", "CSS", "Bootstrap"], true];
+
+for(let i = 0; i < language.length; i++ ){
+    console.log(language[i], typeof language[i]);   // 
+}
