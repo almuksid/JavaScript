314a1d563fb0ae860c6a8c4982d0152164ef9596
Al Muksid <deveelopment.almuksid@gmail.com>
2025-10-15 13:13:37 +0600

    Looping backwards & loops in loops

--- forLoop.js
+++ forLoop.js
@@ -31,3 +31,21 @@ for(let i = 0; i < language.length; i++ ){
     console.log(language[i], typeof language[i]); 
 }
 console.log(types)
+
+
+'strict mode'
+// const language = ["JavaScript", "Python", "C++", ["HTML", "CSS", "Bootstrap"], true, 2000];
+
+for (let i = language.length - 1; i >= 0; i--){
+    console.log(language[i])
+}
+
+// starting exercise
+// Lifting weight repetition
+
+for(let exercise = 1; exercise < 4; exercise++){
+    console.log(`------starting exercise ${exercise}`);
+    for(let rep = 1; rep < 6; rep++){
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+    }
+}
